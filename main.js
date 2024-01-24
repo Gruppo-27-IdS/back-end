81cda00950ba7d716f9d5212dc6dc7d191621e94
Kamicarze73 <andricarzy.03@gmail.com>
2024-01-24 11:34:10 +0100

    Poter accedere da frontend

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -29,7 +30,11 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
+app.use(cors());
 
+// Altri middleware e route possono seguire...
+
+const PORT = 5000;
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
